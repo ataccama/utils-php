0ad687b994642346cd37c31b317122597f9300d5
Miroslav Štolfa <miroslav.stolfa@ataccama.com>
2020-04-14 11:54:23 +0200

    constructors

--- src/env/prototypes/IntegerId.php
+++ src/env/prototypes/IntegerId.php
@@ -20,6 +20,15 @@
         /** @var int */
         protected $id;
 
+        /**
+         * IntegerId constructor.
+         * @param int $id
+         */
+        public function __construct(int $id)
+        {
+            $this->id = $id;
+        }
+
         public function getId(): int
         {
             return $this->id;
--- src/env/prototypes/StringId.php
+++ src/env/prototypes/StringId.php
@@ -20,6 +20,15 @@
         /** @var string */
         protected $id;
 
+        /**
+         * StringId constructor.
+         * @param string $id
+         */
+        public function __construct(string $id)
+        {
+            $this->id = $id;
+        }
+
         public function getId(): string
         {
             return $this->id;
